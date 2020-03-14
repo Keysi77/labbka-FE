6e0c4d030f41aa9ab0f4cb2a46e337d06241f32c
Michal Bella <michal.bella@goodrequest.com>
2020-03-14 20:24:49 +0100

    pridane chybajuce ikonky do assetov, farbicky, upravene styly pre animals page, upravena karticka pre zvieratka, pridana lightbox fgaleria pre detail zvieratka

--- src/components/animal-detail/AnimalDetail.jsx
+++ src/components/animal-detail/AnimalDetail.jsx
@@ -1,53 +1,105 @@
-import React from "react";
+import React, { useState, useEffect, Component } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
 import { selectOneAnimal } from "../../redux/animals/animals.selectors";
-import { get } from "lodash";
+import { get, size } from "lodash";
 import "./AnimalDetail.sass";
 import { Avatar } from "antd";
+import Lightbox from "react-image-lightbox";
+import "react-image-lightbox/style.css";
 
 // TODO: spravit template detail zvieratka
-function AnimalDetail({ animal }) {
-	AnimalDetail.propTypes = {
-		animal: PropTypes.shape(),
-		unsibscribeOneAnimal: PropTypes.func
+class AnimalDetail extends Component {
+	static propTypes = {
+		animal: PropTypes.shape()
 	};
 
-	const shelterAvatar = get(animal, "userRef.avatar");
-	const lat = get(animal.ownerInfo, "address.lat");
-	const lon = get(animal.ownerInfo, "address.lon");
+	state = {
+		photoIndex: 0,
+		isOpen: false
+	};
+	render() {
+		const { animal } = this.props;
+		const shelterAvatar = get(animal, "userRef.avatar");
+		const lat = get(animal.ownerInfo, "address.lat");
+		const lon = get(animal.ownerInfo, "address.lon");
 
-	return (
-		<div className="animal-detail-wrapper">
-			<div className="animal-wrapper">
-				ANIMAL sdfsdf
-				{animal.name}
-				{animal.size}
-				{animal.gender}
-			</div>
-			<div className="right-wrapper">
-				<div className="shelter-wrapper">
-					<div className="shelter-avatar">
-						<Avatar className="avatar" size={92} src={shelterAvatar} />
-					</div>
-					<div className="shelter-name">
-						{get(animal.ownerInfo, "address.city")}
-					</div>
-					<div className="shelter-address">
-						{get(animal.ownerInfo, "address.address")}
-					</div>
-					<div className="shelter-info">
-						<div className="phone">0902 258 687</div>
-						<div className="email">jdfskjdf@gmail.com</div>
+		// const [photoIndex, setPhotoIndex] = useState(0);
+		// const [isOpen, setIsOpen] = useState(false);
+
+		// const [images, setImages] = useState([]);
+		// useEffect(() => {
+		// 	setImages(get(animal, "gallery"));
+		// }, []);
+
+		const gallery = get(animal, "gallery");
+
+		console.log("animal", animal);
+
+		const { photoIndex, isOpen } = this.state;
+
+		return (
+			<div className="animal-detail-wrapper">
+				<div className="animal-wrapper">
+					<div
+						className="animal-image"
+						onClick={() => this.setState({ isOpen: true })}
+					>
+						<img
+							className="animal-covored-photo"
+							src={gallery && gallery[0]}
+							alt="fotka zvieratka"
+						/>
 					</div>
 				</div>
-				IKONKA
-				<img src="/labbka_icon.png" alt="" />
-				<div className="map-wrapper">MAPA</div>
+
+				<div>
+					{isOpen && (
+						<Lightbox
+							mainSrc={gallery[photoIndex]}
+							nextSrc={gallery[(photoIndex + 1) % gallery.length]}
+							prevSrc={
+								gallery[(photoIndex + gallery.length - 1) % gallery.length]
+							}
+							onCloseRequest={() => this.setState({ isOpen: false })}
+							onMovePrevRequest={() =>
+								this.setState({
+									photoIndex: (photoIndex + gallery.length - 1) % gallery.length
+								})
+							}
+							onMoveNextRequest={() =>
+								this.setState({
+									photoIndex: (photoIndex + 1) % gallery.length
+								})
+							}
+						/>
+					)}
+				</div>
+
+				<div className="right-wrapper">
+					<div className="shelter-wrapper">
+						<div className="shelter-avatar">
+							<Avatar className="avatar" size={92} src={shelterAvatar} />
+						</div>
+						<div className="shelter-name">
+							{get(animal.ownerInfo, "address.city")}
+						</div>
+						<div className="shelter-address">
+							{get(animal.ownerInfo, "address.address")}
+						</div>
+						<div className="shelter-info">
+							<div className="phone">0902 258 687</div>
+							<div className="email">jdfskjdf@gmail.com</div>
+						</div>
+					</div>
+					IKONKA
+					<img src="/labbka_icon.png" alt="" />
+					<div className="map-wrapper">MAPA</div>
+				</div>
 			</div>
-		</div>
-	);
+		);
+	}
 }
 
 const mapStateToProps = createStructuredSelector({
--- src/components/animal-item/Animals.jsx
+++ src/components/animal-item/Animals.jsx
@@ -1,143 +1,230 @@
 import PropTypes from "prop-types";
-import React from "react";
+import React, { Component } from "react";
 import { connect } from "react-redux";
-import { Link, Route } from "react-router-dom";
+import { Link } from "react-router-dom";
 // lodash
-import { map, get } from "lodash";
+import { map, get, size } from "lodash";
 // Akcia
 import { fetchOneAnimal } from "../../redux/animals/animals.actions";
-import moment from "moment";
+import TimeAgo from "react-timeago";
+import slovakStrings from "react-timeago/lib/language-strings/sk";
+import buildFormatter from "react-timeago/lib/formatters/buildFormatter";
+// TODO: spravit sharovanie
+// import { FacebookShareButton } from "react-share";
 // Antd
-import { Card, Avatar, Spin, Button, Typography, Row, Col } from "antd";
+import { Avatar, Spin } from "antd";
 // icons
 import locationIcon from "../../assets/icons/location_on.svg";
-import cakeIcon from "../../assets/icons/cake.svg";
-import {
-	EditOutlined,
-	EllipsisOutlined,
-	SettingOutlined,
-	UserOutlined
-} from "@ant-design/icons";
+// ikony do karty
+import breedIcon from "../../assets/icons/plemeno.svg";
+import cakeIcon from "../../assets/icons/torta.svg";
+import genderIcon from "../../assets/icons/pohlavie.svg";
+import sizeIcon from "../../assets/icons/velkost.svg";
+import likeIcon from "../../assets/icons/like_filled.svg";
+import commentIcon from "../../assets/icons/comment.svg";
+import shareIcon from "../../assets/icons/share.svg";
+
+import Lightbox from "react-image-lightbox";
+import "react-image-lightbox/style.css"; // This only needs to be imported once in your app
 
 import "./Animals.sass";
 import { Animated } from "react-animated-css";
 
-const { Meta } = Card;
-const { Title, Text } = Typography;
+const formatter = buildFormatter(slovakStrings);
 
-export function Animals({ animals, fetchOneAnimal }) {
-	Animals.propTypes = {
+class Animals extends Component {
+	static propTypes = {
 		animals: PropTypes.arrayOf(PropTypes.object).isRequired,
 		fetchOneAnimal: PropTypes.func
 	};
 
-	const handleAdoption = id => {
-		fetchOneAnimal(id);
+	state = {
+		photoIndex: 0,
+		isOpen: false
 	};
 
-	const AnimalItems = () => {
-		return animals ? (
-			map(animals, animal => {
-				// <Card
-				// 	key={animal.id}
-				// 	style={{ width: 300 }}
-				// 	className="animal-items"
-				// 	cover={<img alt="example" src={animal.gallery[0]} />}
-				// 	actions={[
-				// 		<SettingOutlined key="setting" />,
-				// 		<EditOutlined key="edit" />,
-				// 		<EllipsisOutlined key="ellipsis" />
-				// 	]}
-				// >
-				// 	<div className="gg">sfsdf</div>
-				// 	<Meta
-				// 		avatar={
-				// 			<Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
-				// 		}
-				// 		title={animal.name}
-				// 		description="This is the description"
-				// 	/>
-				// 	<Link to={"/detail-zvieratka/" + animal.id}>
-				// 		<Button
-				// 			variant="contained"
-				// 			color="primary"
-				// 			onClick={() => handleAdoption(animal.id)}
-				// 		>
-				// 			Viac informacii
-				// 		</Button>
-				// 	</Link>
-				// </Card>
+	render() {
+		const { animals, fetchOneAnimal } = this.props;
+		// const { photoIndex, isOpen } = this.state;
 
-				const createdAt = moment(animal.addedAt).format("YYYY/DD/MM");
-				const avatarLogo = get(animal, "userRef.avatar");
-				return (
-					<div key={animal.id} className="card-wrapper">
-						<div className="card-header">
-							<Avatar
-								className="avatar"
-								shape="square"
-								size={52}
-								src={avatarLogo}
-							/>
-							<div className="shelter-info">
-								<div className="name">{get(animal, "shelter[0].name")}</div>
+		const handleAdoption = id => {
+			fetchOneAnimal(id);
+		};
+
+		// TODO: presunut do utilky
+		const formatYears = year => {
+			if (year === 0) {
+				return "Šteňa";
+			}
+			if (year === 1) {
+				return `${year} rok`;
+			}
+			if (year >= 5) {
+				return `${year} rokov`;
+			}
+			return `${year} roky`;
+		};
+
+		const formatSize = size => {
+			if (size === "SMALL") {
+				return "Malý";
+			}
+			if (size === "MEDIUM") {
+				return "Stredný";
+			}
+			return "Veľký";
+		};
+
+		const AnimalItems = () => {
+			return animals ? (
+				map(animals, animal => {
+					const gallery = get(animal, "gallery");
+					return (
+						<div key={animal.id} className="card-wrapper">
+							<div className="card-header">
+								{/* Utulok info */}
+								<Avatar
+									className="avatar"
+									shape="square"
+									size={52}
+									src={get(animal, "userRef.avatar")}
+								/>
+								<div className="shelter-info">
+									<div className="name">
+										<strong>{get(animal, "shelter[0].name")}</strong>
+									</div>
+								</div>
+							</div>
+							{/* Ikonky info */}
+							<div className="animal-info">
+								<div className="breed background-icon">
+									<img src={breedIcon} alt="plemeno" width="20" />
+									<span>
+										{get(animal, "breedRef.name") === "Neznáme plemeno"
+											? "Neznáme"
+											: get(animal, "breedRef.name")}
+									</span>
+								</div>
+								<div className="age background-icon">
+									<img src={cakeIcon} alt="roky" width="20" />
+									<span>{formatYears(animal.age)}</span>
+								</div>
+								<div className="gender background-icon">
+									<img src={genderIcon} alt="pohlavie" width="20" />
+									<span>
+										{animal.gender === "FEMALE" ? "Samička" : "Samček"}
+									</span>
+								</div>
+								<div className="size background-icon">
+									<img src={sizeIcon} alt="velkost" width="20" />
+									<span>{formatSize(animal.size)}</span>
+								</div>
+							</div>
+							{/* Zvieratko info */}
+							<div className="animal-name">
+								<div className="name">{animal.name}</div>
+								<div className="addedAt">
+									{/* {createdAt} */}
+									<TimeAgo date={animal.addedAt} formatter={formatter} />
+								</div>
+							</div>
+							{/* Fotka */}
+							<div className="animal-photo">
+								<div className="location">
+									<img src={locationIcon} width="16" />
+									<span>{animal.ownerInfo.address.city}</span>
+								</div>
+								<img
+									className="animal-covored-photo"
+									src={animal.gallery[0]}
+									alt="fotka zvieratka"
+								/>
+							</div>
+							{/* Description */}
+							<div className="card-description">
+								<p>{animal.desc}</p>
+							</div>
+							{/* Socialne media + komenty */}
+							<div className="card-social">
+								<div className="likes-and-comments">
+									<div className="likes">
+										<img src={likeIcon} width="16" />
+										<span>{size(animal.likes)}</span>
+									</div>
+									<div className="comments">
+										<img src={commentIcon} width="16" />
+										<span>{size(animal.comments)}</span>
+									</div>
+								</div>
+								<div className="share">
+									<img src={shareIcon} width="16" />
+									Zdielat
+								</div>
+								{/* Lightbox galeria */}
+								{/* TODO: NEfunguje posuva ine obrazky ako by mal */}
+								{/* <div>
+									<button
+										type="button"
+										onClick={() => this.setState({ isOpen: true })}
+									>
+										Open Lightbox
+									</button>
+
+									{isOpen && (
+										<Lightbox
+											mainSrc={gallery && gallery[photoIndex]}
+											nextSrc={gallery[(photoIndex + 1) % gallery.length]}
+											prevSrc={
+												gallery[
+													(photoIndex + gallery.length - 1) % gallery.length
+												]
+											}
+											onCloseRequest={() => this.setState({ isOpen: false })}
+											onMovePrevRequest={() =>
+												this.setState({
+													photoIndex:
+														(photoIndex + gallery.length - 1) % gallery.length
+												})
+											}
+											onMoveNextRequest={() =>
+												this.setState({
+													photoIndex: (photoIndex + 1) % gallery.length
+												})
+											}
+										/>
+									)}
+								</div> */}
+							</div>
+							{/* Presmerovanie */}
+							<div className="card-button">
+								<Link to={"/detail-zvieratka/" + animal.id}>
+									<button onClick={() => handleAdoption(animal.id)}>
+										<span>Viac Informacii</span>
+									</button>
+								</Link>
 							</div>
 						</div>
-						<div className="animal-info">
-							<div className="breed background-icon">
-								{get(animal, "breedRef.name")}
-							</div>
-							<div className="age background-icon">
-								<img src={cakeIcon} alt="narodeniny" />
-								{animal.age} rok
-							</div>
-							<div className="gender background-icon">
-								{animal.gender === "FEMALE" ? "samicka" : "samec"}
-							</div>
-							<div className="size background-icon">{animal.size}</div>
-						</div>
-						<div className="location-icon">
-							<img src={locationIcon} width="16px" />
-							<span>Zilina</span>
-						</div>
-						<div className="animal-photo">
-							<img src={animal.gallery[0]} alt="fotka zvieratka" />
-						</div>
-						<div className="animal-name">
-							<Title level={4}>{animal.name}</Title>
-						</div>
-						{/* <div className="card-description">
-							<Text>{animal.desc}</Text>
-						</div> */}
-						<div className="card-social">likes koments</div>
-						<div className="card-button">
-							<Link to={"/detail-zvieratka/" + animal.id}>
-								<button onClick={() => handleAdoption(animal.id)}>
-									Viac Informacii
-								</button>
-							</Link>
-						</div>
-					</div>
-				);
-			})
-		) : (
-			<Spin />
+					);
+				})
+			) : (
+				<Spin />
+			);
+		};
+
+		return (
+			<Animated
+				animationIn="fadeInRight"
+				animationOut="fadeOutLeft"
+				animationInDuration={1000}
+				animationOutDuration={1000}
+				isVisible={true}
+			>
+				<div className="animals-wrapper">
+					<AnimalItems />
+				</div>
+			</Animated>
 		);
-	};
-
-	return (
-		<Animated
-			animationIn="fadeInRight"
-			animationOut="fadeOutLeft"
-			animationInDuration={1000}
-			animationOutDuration={1000}
-			isVisible={true}
-		>
-			<div className="animals-wrapper">
-				<AnimalItems />
-			</div>
-		</Animated>
-	);
+	}
 }
 const mapDispatchToProps = dispatch => ({
 	fetchOneAnimal: id => dispatch(fetchOneAnimal(id))

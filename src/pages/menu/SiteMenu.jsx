5ee606d742d08ca1913defe29780a428e1e1cce3
Michal Bella <michal.bella@goodrequest.com>
2020-04-13 01:13:43 +0200

    Novy Layout pre SiteMenu + styly

--- src/pages/menu/SiteMenu.jsx
+++ src/pages/menu/SiteMenu.jsx
@@ -4,12 +4,13 @@ import { connect } from "react-redux";
 import PropTypes from "prop-types";
 import { createStructuredSelector } from "reselect";
 import { Layout, Menu, Avatar, Button } from "antd";
+import { get } from "lodash";
 import labbkaLogo from "../../assets/logos/labbka_logo.png";
+
 import {
-	BarChartOutlined,
 	UserOutlined,
-	UploadOutlined,
-	VideoCameraOutlined
+	HomeFilled,
+	CompassFilled
 } from "@ant-design/icons";
 
 import { logoutUser } from "../../redux/auth/auth.actions";
@@ -19,17 +20,65 @@ import "./SiteMenu.sass";
 
 const { Header, Content, Footer, Sider } = Layout;
 
+const PawIcon = () => (
+	<span role="img" className="anticon">
+		<svg
+			fill="currentColor"
+			xmlns="http://www.w3.org/2000/svg"
+			viewBox="0 0 24 24"
+			width="1em"
+			height="1em"
+		>
+			<circle cx="4.5" cy="9.5" r="2.5" />
+			<circle cx="9" cy="5.5" r="2.5" />
+			<circle cx="15" cy="5.5" r="2.5" />
+			<circle cx="19.5" cy="9.5" r="2.5" />
+			<path d="M17.34 14.86c-.87-1.02-1.6-1.89-2.48-2.91-.46-.54-1.05-1.08-1.75-1.32-.11-.04-.22-.07-.33-.09-.25-.04-.52-.04-.78-.04s-.53 0-.79.05c-.11.02-.22.05-.33.09-.7.24-1.28.78-1.75 1.32-.87 1.02-1.6 1.89-2.48 2.91-1.31 1.31-2.92 2.76-2.62 4.79.29 1.02 1.02 2.03 2.33 2.32.73.15 3.06-.44 5.54-.44h.18c2.48 0 4.81.58 5.54.44 1.31-.29 2.04-1.31 2.33-2.32.31-2.04-1.3-3.49-2.61-4.8z" />
+			<path d="M0 0h24v24H0z" fill="none" />
+		</svg>
+	</span>
+);
+
+const ShelterIcon = () => (
+	<span role="img" className="anticon">
+		<svg
+			fill="currentColor"
+			xmlns="http://www.w3.org/2000/svg"
+			viewBox="0 0 24 24"
+			width="1em"
+			height="1em"
+		>
+			<path d="M0 0h24v24H0z" fill="none" />
+			<path d="M20 4H4v2h16V4zm1 10v-2l-1-5H4l-1 5v2h1v6h10v-6h4v6h2v-6h1zm-9 4H6v-4h6v4z" />
+		</svg>
+	</span>
+);
+
+const StoriesIcon = () => (
+	<span role="img" className="anticon">
+		<svg
+			fill="currentColor"
+			xmlns="http://www.w3.org/2000/svg"
+			viewBox="0 0 24 24"
+			width="1em"
+			height="1em"
+		>
+			<path d="M0 0h24v24H0z" fill="none" />
+			<path d="M3 13h8V3H3v10zm0 8h8v-6H3v6zm10 0h8V11h-8v10zm0-18v6h8V3h-8z" />
+		</svg>
+	</span>
+);
+
 const SiteMenu = ({ children, logoutUser, loggedUser }) => {
 	SiteMenu.propTypes = {
 		logoutUser: PropTypes.func,
-		loggedUser: PropTypes.object
+		loggedUser: PropTypes.object,
 	};
 
 	const handleLogOutUser = () => {
 		logoutUser();
 	};
 
-	// TODO: loggedUser po refreshi zmiznu data zo storu
 	return (
 		<Layout>
 			<Sider
@@ -39,58 +88,59 @@ const SiteMenu = ({ children, logoutUser, loggedUser }) => {
 					overflow: "auto",
 					height: "100vh",
 					position: "fixed",
-					left: 0
+					left: 0,
 				}}
+				width={300}
 			>
-				<div className="labbka-logo">
-					<img src={labbkaLogo} alt="Labbka" width="150" height="50" />
+				<div className="avatar">
+				<img src={labbkaLogo} alt="Labbka" width="250"/>
+					<Avatar
+						size={92}
+						src={get(loggedUser, "avatar")}
+						icon={<UserOutlined />}
+					/>
 				</div>
 				<Menu theme="dark" mode="inline">
 					<Menu.Item key="1">
-						<UserOutlined />
+						<HomeFilled />
 						<NavLink to="/">Domov</NavLink>
 					</Menu.Item>
 					<Menu.Item key="2">
-						<VideoCameraOutlined />
+						<PawIcon />
 						<NavLink to="/zvieratka-na-adopciu">Na adopciu</NavLink>
 					</Menu.Item>
 					<Menu.Item key="3">
-						<UploadOutlined />
+						<ShelterIcon />
 						<NavLink to="/utulky">Utulky</NavLink>
 					</Menu.Item>
 					<Menu.Item key="4">
-						<BarChartOutlined />
+						<StoriesIcon />
 						<NavLink to="/pribehy">Pribehy</NavLink>
+					</Menu.Item>
+					<Menu.Item key="5">
+						<CompassFilled />
+						<NavLink to="/stratene">Stratene</NavLink>
 					</Menu.Item>
 				</Menu>
 			</Sider>
-			<Layout className="site-layout" style={{ marginLeft: 200 }}>
-				<Header
-					className="site-layout-background"
-					style={{ paddingLeft: "40px", paddingRight: "40px" }}
-				>
+			<Layout className="site-layout" style={{ marginLeft: 300 }}>
+				<Header>
 					<div className="page-name">Nazov page</div>
+					<div className="page-name">Menu item1 - o nas</div>
+					<div className="page-name">Menu item2 - kontakt</div>
 					<div className="user-info">
 						<div className="user-name">
 							{loggedUser ? loggedUser.name : "Neprihlaseny"}
 						</div>
 						<Button onClick={() => handleLogOutUser()}>Odhlasit</Button>
-						<Avatar size={32} icon={<UserOutlined />} />
+						<Avatar
+							size={32}
+							src={get(loggedUser, "avatar")}
+							icon={<UserOutlined />}
+						/>
 					</div>
 				</Header>
-				<Content
-					style={{
-						margin: "24px 16px 0",
-						overflow: "initial"
-					}}
-				>
-					<div
-						className="site-layout-background content"
-						style={{ padding: 24, textAlign: "center" }}
-					>
-						{children}
-					</div>
-				</Content>
+				<Content className="site-layout-background content">{children}</Content>
 				<Footer style={{ textAlign: "center" }}>Labbka company</Footer>
 			</Layout>
 		</Layout>
@@ -98,11 +148,11 @@ const SiteMenu = ({ children, logoutUser, loggedUser }) => {
 };
 
 const mapStateToProps = createStructuredSelector({
-	loggedUser: selectLoggedUser
+	loggedUser: selectLoggedUser,
 });
 
-const mapDispatchToProps = dispatch => ({
-	logoutUser: () => dispatch(logoutUser())
+const mapDispatchToProps = (dispatch) => ({
+	logoutUser: () => dispatch(logoutUser()),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(SiteMenu);
